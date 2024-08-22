342aff1d1d06ab4501dd4080d0f0b71fafa9c3c8
Chong Wang <wccake@gmail.com>
2024-08-22 11:04:39 +0800

    Update libs.version.toml file && set up  build module

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,101 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.wecanteen105.tmdtv.buildlogic"
+
+// Configure the build-logic plugins to target JDK 17
+// This matches the JDK used to build the project, and is not related to what is running on device.
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+    }
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+//    compileOnly(libs.compose.gradlePlugin)
+//    compileOnly(libs.firebase.crashlytics.gradlePlugin)
+//    compileOnly(libs.firebase.performance.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.room.gradlePlugin)
+    implementation(libs.truth)
+}
+
+tasks {
+    validatePlugins {
+        enableStricterValidation = true
+        failOnWarning = true
+    }
+}
+
+gradlePlugin {
+    plugins {
+//        register("androidApplicationCompose") {
+//            id = "nowinandroid.android.application.compose"
+//            implementationClass = "AndroidApplicationComposeConventionPlugin"
+//        }
+//        register("androidApplication") {
+//            id = "nowinandroid.android.application"
+//            implementationClass = "AndroidApplicationConventionPlugin"
+//        }
+//        register("androidApplicationJacoco") {
+//            id = "nowinandroid.android.application.jacoco"
+//            implementationClass = "AndroidApplicationJacocoConventionPlugin"
+//        }
+//        register("androidLibraryCompose") {
+//            id = "nowinandroid.android.library.compose"
+//            implementationClass = "AndroidLibraryComposeConventionPlugin"
+//        }
+//        register("androidLibrary") {
+//            id = "nowinandroid.android.library"
+//            implementationClass = "AndroidLibraryConventionPlugin"
+//        }
+//        register("androidFeature") {
+//            id = "nowinandroid.android.feature"
+//            implementationClass = "AndroidFeatureConventionPlugin"
+//        }
+//        register("androidLibraryJacoco") {
+//            id = "nowinandroid.android.library.jacoco"
+//            implementationClass = "AndroidLibraryJacocoConventionPlugin"
+//        }
+//        register("androidTest") {
+//            id = "nowinandroid.android.test"
+//            implementationClass = "AndroidTestConventionPlugin"
+//        }
+//        register("hilt") {
+//            id = "nowinandroid.hilt"
+//            implementationClass = "HiltConventionPlugin"
+//        }
+//        register("androidRoom") {
+//            id = "nowinandroid.android.room"
+//            implementationClass = "AndroidRoomConventionPlugin"
+//        }
+//        register("androidFirebase") {
+//            id = "nowinandroid.android.application.firebase"
+//            implementationClass = "AndroidApplicationFirebaseConventionPlugin"
+//        }
+//        register("androidFlavors") {
+//            id = "nowinandroid.android.application.flavors"
+//            implementationClass = "AndroidApplicationFlavorsConventionPlugin"
+//        }
+//        register("androidLint") {
+//            id = "nowinandroid.android.lint"
+//            implementationClass = "AndroidLintConventionPlugin"
+//        }
+//        register("jvmLibrary") {
+//            id = "nowinandroid.jvm.library"
+//            implementationClass = "JvmLibraryConventionPlugin"
+//        }
+    }
+}
---
+++ build-logic/convention/src/main/java/com/wecanteen105/tmdtv/TmdtvFlavor.kt
@@ -0,0 +1,38 @@
+package com.wecanteen105.tmdtv
+
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.ApplicationProductFlavor
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.ProductFlavor
+
+@Suppress("EnumEntryName")
+enum class FlavorDimension {
+    contentType
+}
+
+enum class TmdtvFlavor (val dimension:FlavorDimension, val applicationIdSuffix: String?=null) {
+    demo(FlavorDimension.contentType, applicationIdSuffix = ".demo"),
+    prod(FlavorDimension.contentType)
+}
+
+fun configureFlavors(
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
+    flavorConfigurationBlock: ProductFlavor.(flavor: TmdtvFlavor) -> Unit = {}
+) {
+    commonExtension.apply {
+        flavorDimensions += FlavorDimension.contentType.name
+        productFlavors {
+            TmdtvFlavor.values().forEach {
+                create(it.name) {
+                    dimension = it.dimension.name
+                    flavorConfigurationBlock(this, it)
+                    if (this@apply is ApplicationExtension && this is ApplicationProductFlavor) {
+                        if (it.applicationIdSuffix != null) {
+                            applicationIdSuffix = it.applicationIdSuffix
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

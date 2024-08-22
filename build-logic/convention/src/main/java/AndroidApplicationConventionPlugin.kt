7251e503da7ee558d255a1273ec29ab680b41214
Chong Wang <wccake@gmail.com>
2024-08-22 15:22:37 +0800

    fix a typo: hilt -> lint

--- build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -16,7 +16,7 @@ class AndroidApplicationConventionPlugin: Plugin<Project> {
             with(pluginManager) {
                 apply("com.android.application")
                 apply("org.jetbrains.kotlin.android")
-                apply("tmdtv.android.hilt")
+                apply("tmdtv.android.lint")
                 apply("com.dropbox.dependency-guard")
             }
 
